a6cc60273eeefd4623cb6e0493b9b203c483a4cc
jackytianhappy <1142899950@qq.com>
2016-09-29 14:17:52 +0800

    初始化两个tabbar

--- RouteFrame/RouteFrame/Controllers/Home/HomeVC.m
+++ RouteFrame/RouteFrame/Controllers/Home/HomeVC.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor purpleColor];
+    
+    self.title = @"首页";
 }
 
 - (void)didReceiveMemoryWarning {
--- RouteFrame/RouteFrame/EntranceBase/TabbarVC.m
+++ RouteFrame/RouteFrame/EntranceBase/TabbarVC.m
@@ -22,20 +22,20 @@
     
     HomeVC *homeVC = [[HomeVC alloc]init];
     UINavigationController *homeNavi = [[UINavigationController alloc]initWithRootViewController:homeVC];
-    homeNavi.tabBarItem.image = [[UIImage imageNamed:@"icon_home_normal"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    homeNavi.tabBarItem.selectedImage = [[UIImage imageNamed:@"icon_home_press"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    homeNavi.tabBarItem.title = @"首页";
+    homeVC.tabBarItem.image = [[UIImage imageNamed:@"icon_home_normal"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    homeVC.tabBarItem.selectedImage = [[UIImage imageNamed:@"icon_home_press"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    homeVC.tabBarItem.title = @"首页";
     
     MineVC *mineVC = [[MineVC alloc]init];
     UINavigationController *mineNavi = [[UINavigationController alloc]initWithRootViewController:mineVC];
-    mineNavi.tabBarItem.image = [[UIImage imageNamed:@"icon_me_normal"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    mineNavi.tabBarItem.selectedImage = [[UIImage imageNamed:@"icon_me_press"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    mineNavi.tabBarItem.title = @"我的";
+    mineVC.tabBarItem.image = [[UIImage imageNamed:@"icon_me_normal"]  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    mineVC.tabBarItem.selectedImage = [[UIImage imageNamed:@"icon_me_press"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    mineVC.tabBarItem.title = @"我的";
     
     [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:102.0/255 green:102.0/255 blue:102.0/255 alpha:1.0],NSForegroundColorAttributeName, [UIFont systemFontOfSize:10.0],NSFontAttributeName,nil] forState:UIControlStateNormal];
     [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:224.0/255 green:33.0/255 blue:38.0/255 alpha:1.0],NSForegroundColorAttributeName, [UIFont systemFontOfSize:10.0],NSFontAttributeName,nil] forState:UIControlStateSelected];
     
-    self.viewControllers = @[homeVC,mineVC];
+    self.viewControllers = @[homeNavi,mineNavi];
     
 }
 
